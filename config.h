81275456b25c032d4da6649737b306929b980bec
thehnm <thehnm@posteo.net>
2020-06-27 10:31:25 +0200

    Increase timeout

--- config.h
+++ config.h
@@ -12,7 +12,7 @@ static const char *colorname[NUMCOLS] = {
 static const int failonclear = 1;
 
 /* time in seconds before the monitor shuts down */
-static const int monitortime = 5;
+static const int monitortime = 15;
 
 /* default message */
 static const char * message = "Suckless: Software that sucks less.";
